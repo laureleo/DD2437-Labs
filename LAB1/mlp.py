a9ef9d5d5a115442766e89f9fa1468a550914400
sigis92 <hugringr@gmail.com>
2018-01-30 00:57:39 +0100

    mlp written but something is wrong with the weight updates

--- LAB1/mlp.py
+++ LAB1/mlp.py
@@ -3,18 +3,26 @@ import numpy as np
 def phi(x):
 	return 2/(1 + np.exp(-x)) - 1
 
-def phi_prime(x):
-	return (1 + phi(x)) * (1 - phi(x)) / 2
-
 class MLP(object):
 	def __init__(self, eta, epochs, hidden):
 		self.eta = eta
 		self.epochs = epochs
-
-		#Number of neurons in the hidden layer
 		self.hidden = hidden
 
-	def run(self, X, T):
+	def predict(self, X, T):
+		T = np.array(T)
+		X = np.array(X)
+
+		#Append bias vector to input 
+		bias = np.ones(X.shape[1])
+		X = np.vstack((X, bias))
+		actual = self.forward_pass(X, T)
+		print("actual")
+		print(actual)
+		print("ideal")
+		print(T)
+
+	def learn(self, X, T):
 		T = np.array(T)
 		X = np.array(X)
 
@@ -22,8 +30,13 @@ class MLP(object):
 		bias = np.ones(X.shape[1])
 		X = np.vstack((X, bias))
 
+		#Initialize weights
 		self.init_weights(X, T)
-		self.learn(X,T)
+		
+		for i in range(epochs):
+			x = self.forward_pass(X, T)
+			self.backward_pass(T)
+			self.update_weights(X)
 
 	def init_weights(self, X, T):
 		#V correspond to weights from X to hidden - the first layer
@@ -46,46 +59,52 @@ class MLP(object):
 		for i in range (W_rows):
 			self.W[i] = np.random.normal(0,0.1,W_cols)
 
-	def learn(self, X, T):
-#FORWAD PASS
+	def forward_pass(self, X, T):
 		#Calculate net input to hidden layer
-		H_input = np.dot(self.V, X)
+		self.H_input = np.dot(self.V, X)
 
 		#Apply the activation function
-		for i in range(H_input.shape[0]):
-			for j in range(H_input.shape[1]):
-				H_input[i,j] = phi(H_input[i,j])
+		for i in range(self.H_input.shape[0]):
+			for j in range(self.H_input.shape[1]):
+				self.H_input[i,j] = phi(self.H_input[i,j])
 
 		#Create and append the bias vector to the input for the output layer
-		bias = np.ones(H_input.shape[1])
-		H_output = np.vstack((H_input, bias))
+		bias = np.ones(self.H_input.shape[1])
+		self.H_output = np.vstack((self.H_input, bias))
 
 		#Calculate net input to output layer
-		O_input = np.dot(self.W, H_output)
+		self.O_input = np.dot(self.W, self.H_output)
 
 		#Apply the activation function
-		for i in range(O_input.shape[0]):
-				for j in range(O_input.shape[1]):
-					O_input[i,j] = phi(O_input[i,j])
+		for i in range(self.O_input.shape[0]):
+			for j in range(self.O_input.shape[1]):
+				self.O_input[i,j] = phi(self.O_input[i,j])
 
-#BACKWARD PASS
+		actual = self.O_input
+		return actual
+
+	def backward_pass(self, T):
 		#Compute error on output layer
-		error1 = O_input - T
-		O_derivative = np.multiply((1 + O_input), (1 - O_input)) * 0.5
-		delta_O = np.multiply(error1, O_derivative)
+		error1 = self.O_input - T
+		self.O_derivative = np.multiply((1 + self.O_input), (1 - self.O_input)) * 0.5
+		self.delta_O = np.multiply(error1, self.O_derivative)
 
 		#Compute error on hidden layer
-		error2 = np.dot(np.transpose(self.W),delta_O)
-		H_derivative = np.multiply((1 + H_output), (1 - H_output)) * 0.5
-		delta_H = np.multiply(error2, H_derivative)
+		error2 = np.dot(np.transpose(self.W), self.delta_O)
+		self.H_derivative = np.multiply((1 + self.H_output), (1 - self.H_output)) * 0.5
+		self.delta_H = np.multiply(error2, self.H_derivative)
 
 		#Remove the last row which contains the bias
-		delta_H = np.delete(delta_H, delta_H.shape[0] -1, 0)
+		self.delta_H = np.delete(self.delta_H, self.delta_H.shape[0] -1, 0)
 
-#WEIGHT UPDATE
+	def update_weights(self, X):
+		print(self.W)
+		self.W = eta * self.W -(1 - eta) * np.dot(self.delta_O, np.transpose(self.H_output))
+		print(self.W)
+		self.V = eta * self.V -(1 - eta) * np.dot(self.delta_H, np.transpose(X))
 
-eta = 0.01
-epochs = 100
+eta = 0.9
+epochs = 10
 hidden = 3
 
 in1 =[
@@ -117,10 +136,16 @@ in3 = [
 out3= [[-1,-1, 1],[1,-1,1]]
 
 
+in4 = [
+	[1],
+	[1]
+	]
+out4 = [[1]]
 	
 
 MLP = MLP(eta, epochs, hidden)
-MLP.run(in3, out3)
+MLP.learn(in1, out1)
+MLP.predict(in4, out4)
 
 
 
