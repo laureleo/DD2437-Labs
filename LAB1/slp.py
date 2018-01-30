9d2f2924e0984fb07685dec261d030ec85e67356
sigis92 <hugringr@gmail.com>
2018-01-30 11:29:01 +0100

    added predictive function to slp. reordered functions

--- LAB1/slp.py
+++ LAB1/slp.py
@@ -6,29 +6,63 @@ class Perceptron(object):
 		self.eta = eta
 		self.epochs = epochs
 
-	def run_pct(self, x, t):
-		#np.array just ensures that our inputs are recognized as matrices by numpy
-		T = np.array(t)
-		X = np.array(x)
+
+	def run_pct(self, X, T):
+		self.setup(X, T)
+		self.init_weights()
+		self.learn()
+
+
+	def setup(self, X, T):
+		#Convert input into a format numpy can deal with
+		self.T = np.array(T)
+		self.X = np.array(X)
 
 		#Fill a one-dimensional array (with the same number of cols as X) with 1:s. This is the bias vector
-		bias = np.ones(X.shape[1])
+		bias = np.ones(self.X.shape[1])
 
 		#Append the bias vector as the last row in the input matrix
-		X = np.vstack((X, bias))
+		self.X = np.vstack((self.X, bias))
 
-		#Initialize the weights, do the learning and return the final weight matrix
-		self.init_weights(X, T)
-		self.learn(X, T)
-		return self.W
 
-	def learn(self, X, T):
+	def init_weights(self):
+		W_rows = output_dimensionality = self.T.shape[0]
+		W_cols = input_dimensionality = self.X.shape[0]
 
+		#Initialize a weight matrix with as many rows as the output dimensionality, as many cols as input dimensionality
+		self.W = np.zeros(shape=(W_rows, W_cols))
+		
+		#Fill each row with values drawn randomly from a normal distribution with a stdev of 0.1
+		for i in range (W_rows):
+			self.W[i] = np.random.normal(0, 0.1, W_cols)
+
+
+	
+	def predict(self, X, T):
+		self.setup(X, T)
+		net_input = np.dot(self.W, self.X)
+		for i in range(net_input.shape[0]):
+				for j in range (net_input.shape[1]):
+					if net_input[i,j] >= 0:
+						net_input[i,j] =  1
+					else:
+						net_input[i,j] =  -1
+		print("Predicted")
+		print(net_input)
+		print("Actual is")
+		print(self.T)
+		print("Error is")
+		print(net_input - self.T)
+		return(net_input)
+		
+
+
+	def learn(self):
 		#For each epoch
 		for i in range(self.epochs):
 
 			#Calculate net input to neuron
-			net_input = np.dot(self.W, X)
+			net_input = np.dot(self.W, self.X)
 			
 			#For each element, apply the activation function
 			for i in range(net_input.shape[0]):
@@ -39,28 +73,16 @@ class Perceptron(object):
 						net_input[i,j] =  -1
 					
 			#Get the error
-			error = net_input - T
+			error = net_input - self.T
 
 			#Apply the perceptron learning rule
-			update = -1 * self.eta * np.dot(error, np.transpose(X))
+			update = -1 * self.eta * np.dot(error, np.transpose(self.X))
 
 			#Update weights 
 			self.W += update 
 
-	def init_weights(self, X, T):
-		W_rows = output_dimensionality = T.shape[0]
-		W_cols = input_dimensionality = X.shape[0]
 
-		#Initialize a weight matrix with as many rows as the output dimensionality, as many cols as input dimensionality
-		self.W = np.zeros(shape=(W_rows, W_cols))
-		
-		#Fill each row with values drawn randomly from a normal distribution with a stdev of 0.1
-		for i in range (W_rows):
-			self.W[i] = np.random.normal(0, 0.1, W_cols)
-
-#Test running the perceptron
-eta = 0.01
-epochs = 10
+#Training samples
 in1 =[
 	[1,1,-1,-1],
 	[1,-1,1,-1]
@@ -89,6 +111,9 @@ in3 = [
 
 out3= [[-1,-1, 1],[1,-1,1]]
 
+#Run the perceptron
+eta = 0.01
+epochs = 100
 perceptron = Perceptron(eta, epochs)
-print("Weights are")
-print(perceptron.run(in1, out1))
+perceptron.run_pct(in1, out1)
+perceptron.predict(in1, out1)
