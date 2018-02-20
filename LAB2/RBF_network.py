0a115e2080cac229f99d9166b93607f706df9f8b
mariono96 <olivier.marion1996@gmail.com>
2018-02-20 22:04:16 +0100

    some more lab2. functional (fingers-crossed) impl for parts 3.1 3.2 3.3

--- LAB2/RBF_network.py
+++ LAB2/RBF_network.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 
 class RBF_network(object):
     
-    def __init__(self,  n, div=15):
+    def __init__(self,  n, var=15):
         self.weights = np.random.randn(n)/100
         self.mean = np.linspace(-1,1,n)
-        self.variance = np.ones(n)/div #np.ones(n)/n for batch works well
+        self.variance = np.ones(n)*var #np.ones(n)/n for batch works well
         self.n = n
         
     def phi(self, x, i):
@@ -27,17 +27,20 @@ class RBF_network(object):
         phi_mtx = np.array([[  self.phi(x[i], j)  for j in range(self.n)]    for i in range(len(x))])
         self.weights = np.array(np.linalg.lstsq(phi_mtx, f)[0])
         
-    def learning_incr(self, x, f, eta ):
-        diff_min_max = np.abs(np.max(x) - np.min(x))*4
+    def learning_incr(self, x, f, eta, error_bound = 0.01, noite=10 ):
+        diff_min_max = np.abs(np.max(x) - np.min(x))
         self.mean = np.linspace(np.min(x)-diff_min_max/4, np.max(x)+diff_min_max/4, self.n)
-        for k in range(len(x)):
-            phi_vect = np.array( [self.phi(x[k], i) for i in range(self.n)])
-            #error = 1/2 ( f[k] - np.sum(self.weights * phi_vect) )**2
-            
-            d_w = eta* (f[k] - np.sum(self.weights * phi_vect)) * phi_vect 
-            self.weights +=d_w
-            
-       
+        error = np.average(np.sum((np.sin(2*x) - self.output(x))**2))
+        ite=0
+        while error > error_bound and ite < noite:
+            for k in range(len(x)):
+                phi_vect = np.array( [self.phi(x[k], i) for i in range(self.n)])
+                #error = 1/2 ( f[k] - np.sum(self.weights * phi_vect) )**2
+                
+                d_w = eta* (f[k] - np.sum(self.weights * phi_vect)) * phi_vect 
+                self.weights +=d_w
+            error = np.average(np.sum((np.sin(2*x) - self.output(x))**2))
+            ite+=1
         
     def output(self, x):
         o = np.zeros(len(x))
--- LAB2/RBF_test.py
+++ LAB2/RBF_test.py
@@ -7,32 +7,42 @@ Created on Sun Feb 18 16:23:40 2018
 import matplotlib.pyplot as plt
 import numpy as np
 from RBF_network import RBF_network
-net = RBF_network(100)
+net = RBF_network(100, 0.01)
 
 
-x = np.arange(0, 2*np.pi, 0.01)
+x = np.arange(0, 2*np.pi, 0.1)
 f = np.sin(2*x)
 
-net.learning_incr(x, f, 0.007)
-#rangeE = [0.007]#np.linspace(0.001, 0.01, 100)
-#rangeV = np.linspace(1, 100, 50)
-#MSE = np.zeros(50)
+def func(x):
+    if x >=0:
+        return 1
+    else:
+        return -1
+#f = np.vectorize(func)(f)
+#net.learning_batch(x,f)
+net.learning_incr(x, f, 0.004, 0.001, 1000) #O.O41 is a good one for 100
+#0.009 for 1000 nodes
+#0.0009 for 10000 nodes
+#rangeE = np.linspace(0.0001, 0.001, 10)
+##rangeV = np.linspace(1, 100, 50)
+#MSE = np.zeros(10)
 #i=0
-#for div in rangeV:
+#for eta in rangeE:
 #    np.random.seed(9001)
-#    net = RBF_network(100, div)
-#    net.learning_incr(x, f, 0.007)
+#    net = RBF_network(10000)
+#    net.learning_incr(x, f, eta)
 #    test_x = x+  0.05
 #    o = net.output(test_x)
 #    MSE[i] = np.average(np.sum((np.sin(2*test_x) - o)**2))
 #    i+=1
-#
-#plt.plot(rangeV, MSE)
+#plt.figure()
+#plt.plot(rangeE, MSE)
 #plt.show()
 
 plt.figure()
-test_x = x
+test_x = x + 0.05
 o = net.output(test_x)
 plt.plot(test_x, np.sin(2*test_x), linewidth=5)
 plt.plot(test_x, o)
+absolute_residual_error = np.average(np.abs(np.sin(2*test_x) - o))
 plt.show()
