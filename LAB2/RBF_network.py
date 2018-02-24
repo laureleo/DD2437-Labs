d75af29e8aac9a32a53a05cc2d732257be6453a7
mariono96 <olivier.marion1996@gmail.com>
2018-02-24 13:41:25 +0100

    whatever

--- LAB2/RBF_network.py
+++ LAB2/RBF_network.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 class RBF_network(object):
     
-    def __init__(self,  n, var=15):
+    def __init__(self,  n, var=0.01):
         self.weights = np.random.randn(n)/100
         self.mean = np.linspace(-1,1,n)
         self.variance = np.ones(n)*var #np.ones(n)/n for batch works well
@@ -22,14 +22,14 @@ class RBF_network(object):
         return np.exp ( -(x-self.mean[i])**2/(2*self.variance[i]))
     
     def learning_batch(self, x, f):
-        diff_min_max = np.abs(np.max(x) - np.min(x))
-        self.mean = np.linspace(np.min(x)-diff_min_max/4, np.max(x)+diff_min_max/4, self.n)
+        diff_min_max = np.abs(np.max(x) - np.min(x))*0.1 #not required
+        self.mean = np.linspace(np.min(x)-diff_min_max, np.max(x)+diff_min_max, self.n)
         phi_mtx = np.array([[  self.phi(x[i], j)  for j in range(self.n)]    for i in range(len(x))])
         self.weights = np.array(np.linalg.lstsq(phi_mtx, f)[0])
         
     def learning_incr(self, x, f, eta, error_bound = 0.01, noite=10 ):
-        diff_min_max = np.abs(np.max(x) - np.min(x))
-        self.mean = np.linspace(np.min(x)-diff_min_max/4, np.max(x)+diff_min_max/4, self.n)
+        diff_min_max = np.abs(np.max(x) - np.min(x))*0.33
+        self.mean = np.linspace(np.min(x)-diff_min_max, np.max(x)+diff_min_max, self.n)
         error = np.average(np.sum((np.sin(2*x) - self.output(x))**2))
         ite=0
         while error > error_bound and ite < noite:
