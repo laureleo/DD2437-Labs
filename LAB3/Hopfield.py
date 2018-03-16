f4d25f0068665200ad53a397d9aae8b077119208
mariono96 <olivier.marion1996@gmail.com>
2018-03-16 20:08:43 +0100

    some of 3.2, needs to try selecting units randomly for update

--- LAB3/Hopfield.py
+++ LAB3/Hopfield.py
@@ -13,7 +13,7 @@ class Hopfield(object):
         self.W = np.zeros((N,N))
         
     #Learning made with a list of patterns and with a possibility not to scale the weights
-    def Hebbian_learning(self, patterns_list, scale=True):
+    def Hebbian_learning(self, patterns_list, scale=False): #false if just sign
         P = len(patterns_list)
         for i in range(self.N):
             for j in range(self.N):
@@ -21,10 +21,11 @@ class Hopfield(object):
                     self.W[i][j]+= patterns_list[mu][i]*patterns_list[mu][j]
                 if scale:
                     self.W[i][j]/=self.N
+        
             
     #def Network_recall(self, list_x):
     def sign(self, x):
-        if x >=0:
+        if x >= 0:
             return(1)
         else: 
             return(-1)
@@ -44,68 +45,69 @@ class Hopfield(object):
 ##===========================================================================##
 
 
-##2.2 
-l = []
-net = Hopfield(8)
-x1=[-1, -1, 1, -1, 1, -1, -1, 1]
-x2=[-1, -1, -1, -1, -1, 1, -1, -1]
-x3=[-1, 1, 1, -1, -1, 1, -1, 1]
-l.append(x1); l.append( x2); l.append( x3)
-
-net.Hebbian_learning(l, scale=False)
-#for x in l:
-#    print("vector {} and update rule {}".format(x, net.update_rule(x)))
-#    print("diff={}".format(x - net.update_rule(x)))
-    
-##3.1 Convergence and attractors
-x1d=[ 1, -1, 1, -1, 1, -1, -1, 1] #converges to x1
-x2d=[ 1, 1, -1, -1, -1, 1, -1, -1] #converges to smth unknown
-x3d=[ 1, 1, 1, -1, 1, 1, -1, 1] #converges to x3
-
-l2 = [x1d, x2d, x3d]
-for x in l2:
-    entry = x
-    out = net.update_rule(entry)
-    while not np.array_equal(entry, out):
-        entry =out
-        out = net.update_rule(entry)
-    print("vec:{} and fix point:{}".format(x, out))
-    
-attractors = [x1, x2, x3]
-#very greedy method to find the attractors by trying everypossibility
-trial = -np.ones(8)
-trials = []
-a = 1; b =1; c=1; d=1; e=1; g=1; f=1; h=1
-for i in range(2):
-    a =-a
-    for j in range(2):
-        b=-b
-        for k in range(2):
-            c=-c
-            for l in range(2):
-                d=-d
-                for m in range(2):
-                    e=-e
-                    for n in range(2):
-                        f=-f
-                        for o in range(2):
-                            g=-g
-                            for p in range(2):
-                                h=-h
-                                trials.append([a,b,c,d,e,f,g,h])
-
-
-
-n =0
-for trial in trials:
-    if (np.array_equal(net.update_rule(trial), trial)):
-        if trial not in attractors:
-            attractors.append(trial)
-
-    
-print("the attractors are")
-for x in attractors:
-    print(x)
-    
-
-
+###2.2 
+#l = []
+#net = Hopfield(8)
+#x1=[-1, -1, 1, -1, 1, -1, -1, 1]
+#x2=[-1, -1, -1, -1, -1, 1, -1, -1]
+#x3=[-1, 1, 1, -1, -1, 1, -1, 1]
+#l.append(x1); l.append( x2); l.append( x3)
+#
+#net.Hebbian_learning(l, scale=False)
+##for x in l:
+##    print("vector {} and update rule {}".format(x, net.update_rule(x)))
+##    print("diff={}".format(x - net.update_rule(x)))
+#    
+###3.1 Convergence and attractors
+#x1d=[ 1, -1, 1, -1, 1, -1, -1, 1] #converges to x1
+#x2d=[ 1, 1, -1, -1, -1, 1, -1, -1] #converges to smth unknown
+#x3d=[ 1, 1, 1, -1, 1, 1, -1, 1] #converges to x3
+#
+#l2 = [x1d, x2d, x3d]
+#for x in l2:
+#    entry = x
+#    out = net.update_rule(entry)
+#    while not np.array_equal(entry, out):
+#        entry =out
+#        out = net.update_rule(entry)
+#    print("vec:{} and fix point:{}".format(x, out))
+# 
+##How many attractors are there in this network
+#attractors = [x1, x2, x3]
+##very greedy method to find the attractors by trying everypossibility
+#trial = -np.ones(8)
+#trials = []
+#a = 1; b =1; c=1; d=1; e=1; g=1; f=1; h=1
+#for i in range(2):
+#    a =-a
+#    for j in range(2):
+#        b=-b
+#        for k in range(2):
+#            c=-c
+#            for l in range(2):
+#                d=-d
+#                for m in range(2):
+#                    e=-e
+#                    for n in range(2):
+#                        f=-f
+#                        for o in range(2):
+#                            g=-g
+#                            for p in range(2):
+#                                h=-h
+#                                trials.append([a,b,c,d,e,f,g,h])
+#
+#
+#
+#n =0
+#for trial in trials:
+#    if (np.array_equal(net.update_rule(trial), trial)):
+#        if trial not in attractors:
+#            attractors.append(trial)
+#
+#
+#print("the attractors are")
+#for x in attractors:
+#    print(x)
+#    
+#
+#
---
+++ LAB3/SequentialUpdate.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 16 17:15:05 2018
+
+@author: Olivier
+"""
+import numpy as np
+from Hopfield import *
+import matplotlib.pyplot as plt 
+
+net = Hopfield(1024)
+pict = np.loadtxt("pict.dat", dtype='i', delimiter=',')
+pict = np.reshape(pict, (11, 1024))
+learn = []
+learn.append(pict[0]); learn.append(pict[1]), learn.append(pict[2])
+net.Hebbian_learning(learn, scale=False)
+
+
+
+#Three patterns are stable
+#for x in learn:
+#    plt.figure()
+#    plt.subplot(121)
+#    xp = np.reshape(x, (32, 32))
+#    plt.imshow(xp)
+#    plt.subplot(122)
+#    y = net.update_rule(x)
+#    yp = np.reshape(y, (32, 32))
+#    plt.imshow(yp)
+#plt.show()
+
+#p10 ?
+plt.figure()
+y = net.update_rule(pict[9])
+plt.subplot(131)
+plt.imshow(np.reshape(pict[9], (32, 32)))
+plt.subplot(132)
+plt.imshow(np.reshape(y, (32, 32)))
+plt.subplot(133)
+plt.imshow(np.reshape(pict[0], (32, 32)))
+
+#p11
+plt.figure()
+plt.subplot(131)
+plt.imshow(np.reshape(pict[1], (32, 32)))
+plt.subplot(132)
+plt.imshow(np.reshape(pict[2], (32, 32)))
+plt.subplot(133)
+plt.imshow(np.reshape(pict[10], (32, 32)))
+
+y = net.update_rule(pict[10])
+inp = y
+while not np.array_equal(inp,  net.update_rule(inp)):
+    inp = net.update_rule(inp)
+plt.figure()
+plt.imshow(np.resize(inp, (32, 32)))
+
+
